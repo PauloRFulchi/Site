685ec18c97c889ce74c4ba39eb170dae74071e66
Paulo R Fulchi <93952004+PauloRFulchi@users.noreply.github.com>
2021-12-14 16:20:35 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,7 @@
-
-
 var btnPdf = document.querySelector(".btnPdf");
 btnPdf.addEventListener("click", function(){
     
-    window.open("Currículo.pdf");
-
-});
-
-
-var btnflag_usa = document.querySelector(".flag_usa");
-btnflag_usa.addEventListener("click", function(){
-    
-    Mensagem("USA");
-
-});
-
-var btnflag_bra = document.querySelector(".flag_bra");
-btnflag_bra.addEventListener("click", function(){
-    
-     Mensagem("BRA");
-
-});
-
-var btnflag_ita = document.querySelector(".flag_ita");
-btnflag_ita.addEventListener("click", function(){
-    
-     Mensagem('ITA');
+    window.open("Currículo.pdf");   
 
 });
 
@@ -47,3 +23,12 @@ function screenSize(){
     }
 }
 
+function Mensagem(par) {
+    var x = document.getElementById("snackbar");
+    x.innerHTML = par;
+    x.className = "show";
+
+    setTimeout(function(){ 
+        x.className = x.className.replace("show", ""); 
+    }, 3000);
+}
