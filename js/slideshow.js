8c48f56f33e632cc0236cea3fcfd43d622228eaf
Paulo R Fulchi <93952004+PauloRFulchi@users.noreply.github.com>
2022-01-03 18:52:06 -0300

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -15,10 +15,10 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("dot");
 
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }    
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   
   for (i = 0; i < slides.length; i++) {
